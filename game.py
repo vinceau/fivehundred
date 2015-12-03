fe1f10b08975d28a9cc8eba9b6ea96524f2c2808
Vince Au <vinceau09@gmail.com>
2015-12-04 09:19:02 +1100

    Use players for order and player_map for objects

--- game.py
+++ game.py
@@ -5,22 +5,31 @@ from lib500.cards import Player, Pile
 
 class Game(object):
     def __init__(self):
-        self.players = []
-        self.started = False
+        self.players = [] #list of player names in the order they play
+        self.player_map = {} #mapping from player names to player objects
         self.kitty = Pile()
 
     def add_player(self, name):
-        self.players.append(Player(name))
+        #this is the order of play
+        #player 1 and 3 are assumed to be on a team
+        if name in self.players:
+            print('That name is already in use!')
+            return
+        self.players.append(name)
+        self.player_map[name] = Player(name)
 
     def start(self):
         no_players = len(self.players)
         if no_players != 4 and no_players != 6:
             print('You don\'t have the right number of people!')
             return
-        deck = FiveHundredDeck(no_players == 6)
+        self._deal()
+
+    def _deal(self):
+        deck = FiveHundredDeck(len(self.players) == 6)
         for i in [3, 4, 3]:
-            for player in self.players:
-                player.draw(deck, i)
+            for name in self.players:
+                self.player_map[name].draw(deck, i)
             self.kitty.add(deck.get_top())
 
 def main():
