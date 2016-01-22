a0c85963e5c020e3b0977db1fbce1c92b4cbb093
Vince Au <vinceau09@gmail.com>
2016-01-23 00:42:02 +1100

    Don't repeat yourself with 500 deck generation

--- fivehundred/core.py
+++ fivehundred/core.py
@@ -10,11 +10,11 @@ class FiveHundredDeck(Pile):
     def __init__(self, sixplayer):
         Pile.__init__(self)
         self.cards = []
+        self._four_player()
         if sixplayer:
             self._six_player()
-        else:
-            self._four_player()
         self.shuffle()
+        assert self.size() == (63 if sixplayer else 43)
 
     def _four_player(self):
         values = {
@@ -24,10 +24,10 @@ class FiveHundredDeck(Pile):
             '8' : Value('8', 'Eight', 8),
             '9' : Value('9', 'Nine', 9),
             '10': Value('10', 'Ten', 10),
-            'J' : Value('J', 'Jack', 11),
-            'Q' : Value('Q', 'Queen', 12),
-            'K' : Value('K', 'King', 13),
-            'A' : Value('A', 'Ace', 14),
+            'J' : Value('J', 'Jack', 14),
+            'Q' : Value('Q', 'Queen', 15),
+            'K' : Value('K', 'King', 16),
+            'A' : Value('A', 'Ace', 17),
         }
         for s in suits.values():
             for v in values.values():
@@ -41,20 +41,8 @@ class FiveHundredDeck(Pile):
         values = {
             '2' : Value('2', 'Two', 2),
             '3' : Value('3', 'Three', 3),
-            '4' : Value('4', 'Four', 4),
-            '5' : Value('5', 'Five', 5),
-            '6' : Value('6', 'Six', 6),
-            '7' : Value('7', 'Seven', 7),
-            '8' : Value('8', 'Eight', 8),
-            '9' : Value('9', 'Nine', 9),
-            '10': Value('10', 'Ten', 10),
             '11': Value('11', 'Eleven', 11),
             '12': Value('12', 'Twelve', 12),
-            #save a spot for thirteen
-            'J' : Value('J', 'Jack', 14),
-            'Q' : Value('Q', 'Queen', 15),
-            'K' : Value('K', 'King', 16),
-            'A' : Value('A', 'Ace', 17),
         }
         for s in suits.values():
             for v in values.values():
@@ -62,4 +50,6 @@ class FiveHundredDeck(Pile):
         val_thirteen = Value('13', 'Thirteen', 13)
         self.add(PlayingCard(suits['diamonds'], val_thirteen))
         self.add(PlayingCard(suits['hearts'], val_thirteen))
-        self.add(Birdie())
+        val_four = Value('4', 'Four', 4)
+        self.add(PlayingCard(suits['spades'], val_four))
+        self.add(PlayingCard(suits['clubs'], val_four))
