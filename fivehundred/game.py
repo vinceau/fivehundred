4e41287bd98231f577302b331967b7d6a48c6d7e
Vince Au <vinceau09@gmail.com>
2016-01-23 00:03:05 +1100

    Use a Round object to keep track of rounds.
    Instead of globals that get reset each round, Game will create
    Rounds to keep track of the round, bidding, etc.

--- fivehundred/game.py
+++ fivehundred/game.py
@@ -1,9 +1,22 @@
 """The game handler
 """
-from random import shuffle
 from .core import FiveHundredDeck
-from .bids import valid_bid, bid_value, Bid
+from .bids import valid_bid, Bid
 from .cards import Player, Pile
+
+class NotEnoughPlayersException(Exception):
+    def __init__(self):
+        Exception.__init__(
+            self,
+            "You need either 4 or 6 players for a game of 500."
+        )
+
+class SamePlayerNameException(Exception):
+    def __init__(self):
+        Exception.__init__(
+            self,
+            "Players need to have different names."
+        )
 
 class Order(object):
     """Keeps track of the order, who has delt etc.
@@ -25,136 +38,102 @@ class Order(object):
         """
         self.start += 1
 
-class Game(object):
-    def __init__(self):
-        self.players = [] #list of player names in the order they play
-        self.player_map = {} #mapping from player names to player objects
+
+class Round(object):
+    """This will manage the round. Shuffle the deck, handle the bidding, handle
+    the play, and utlimately return when the 10 tricks are over with the
+    winning team and the amount of points earned or lost.
+    """
+    def __init__(self, players, dealer=0):
+        self.players = players
+        #mapping from player names to player objects
+        self.player_map = {}
+        for p in players:
+            self.player_map[p] = Player(p)
         self.kitty = Pile()
-        self.team_one = []
-        self.team_two = []
-        self.dealer = 0
-        self.bidders = []
         self.state = 'initialised'
-        self.current_bidder = 0
-        self.winning_bid = None
-        self.trumpsuit = None
+        self.turn = None #who's turn is it?
+        self.bidders = [] #who's still in the bid?
+        self.bid = (None, None) #tuple of (player, bid)
+        self._deal(dealer)
+        self._start_bidding((dealer + 1) % len(players))
 
-    def add_player(self, name):
-        if not self.state == 'initialised':
-            return self._wrong_state()
-        #this is the order of play
-        #player 1 and 3 are assumed to be on a team
-        if name in self.players:
-            print('That name is already in use!')
-            return
-        self.players.append(name)
-        self.player_map[name] = Player(name)
-
-
-    def add_team(self, team):
-        if not self.state == 'initialised':
-            return self._wrong_state()
-        team_size = len(team)
-        #ensure teams are of the right size
-        if team_size != 2 and team_size != 3:
-            print('Teams must be of two or three!')
-            return
-        #ensure players have unique names
-        name_used = any(name in self.players for name in team)
-        if len(team) != len(set(team)) or name_used:
-            print('Players need to have different names!')
-            return
-        #handle first team
-        if not self.team_one:
-            #add first team
-            self.team_one = team
-            for n in team:
-                self.player_map[n] = Player(n)
-            return
-        #handle second team
-        if team_size != len(self.team_one):
-            print('The two teams need to be of the same size!')
-            return
-        #teams are of the same size
-        self.team_two = team
-        #randomise the order
-        shuffle(self.team_one)
-        shuffle(self.team_two)
-        for i in range(team_size):
-            self.players.append(self.team_one[i])
-            self.players.append(self.team_two[i])
-
-    def start(self):
-        no_players = len(self.players)
-        if no_players != 4 and no_players != 6:
-            print('You don\'t have the right number of people!')
-            return
-        self._deal()
-        self._bidding()
-
-    def _deal(self):
-        deck = FiveHundredDeck(len(self.players) == 6)
-        for i in [3, 4, 3]:
-            for name in self.players:
-                self.player_map[name].draw(deck, i)
-            self.kitty.add(deck.get_top())
-
-    def _bidding(self):
-        """Start the bidding process
+    def _deal(self, dealer):
+        """Creates a deck and deals cards to each player
         """
-        bid_order = Order(self.players, self.dealer + 1)
-        self.bidders = list(bid_order)
+        no_players = len(self.players)
+        deck = FiveHundredDeck(no_players == 6)
+        assert deck.size() == 10 * no_players + 3
+        for j in [3, 4, 3]:
+            for i in range(no_players):
+                p = self.players[(dealer + i + 1) % no_players]
+                self.player_map[p].draw(deck, j)
+            self.kitty.add(deck.get_top())
+        assert deck.size() == 0
+
+    def _start_bidding(self, first_bidder):
+        """Starts the bidding process.
+        """
         self.state = 'bidding'
+        self.bidders = list(self.players)
+        self.turn = self.players[first_bidder]
 
     def set_bid(self, player, bid):
+        """If bid is valid, set the bid to bid
+        """
         #are we in bidding phase?
         if self.state != 'bidding':
-            return self._wrong_state()
+            print('We\'re not in the bidding state anymore.')
+            return False
         #is it the right player making the bid?
-        if player != self.bidders[self.current_bidder]:
+        if player != self.turn:
             print('It is not your turn to bid!')
             return False
         #is it a valid bid?
         if not valid_bid(bid):
             print('That bid is not valid')
             return False
-        new_bid = Bid(bid)
+
+        if bid != 'PASS':
+            new_bid = Bid(bid)
+            #is the bid high enough?
+            if self.bid[1] is not None and self.bid[1] >= new_bid:
+                print('You need to bid higher than that!')
+                return False
+            #cool the bid was valid so update bid
+            self.bid = (player, new_bid)
+
+        next_bidder = (self.bidders.index(self.turn) + 1) % len(self.bidders)
+        self.turn = self.bidders[next_bidder]
+
         #did someone pass? do we have a winner?
         if bid == 'PASS':
-            del self.bidders[self.current_bidder]
+            self.bidders.remove(player)
             if len(self.bidders) == 1:
-                #wooh! finally done
-                #distribute kitty
-                self.trumpsuit = self.winning_bid.suit
-                self.bidders[0].hand.extend(self.kitty)
+                self.turn = self.bidders[0]
+                self.player_map[self.turn].hand.extend(self.kitty)
                 self.kitty.empty()
                 self.state = 'kitty'
-                return True
-        #is the bid high enough?
-        elif self.winning_bid >= new_bid:
-            print('You need to bid higher than that!')
-            return False
-        #yep valid bid
-        else:
-            self.current_bidder += 1
-            self.winning_bid = new_bid
-        #rewrap bidder if necessary
-        if self.current_bidder > len(self.players):
-            self.current_bidder = 0 #back to start
         return True
 
-    def drop(self, cards):
-        if self.state != 'kitty':
-            return self._wrong_state()
 
-    def _wrong_state(self):
-        print('There\'s a time and place for everything.')
-        return False
+class Game(object):
+    """Takes a list of players. That's the order that play will be in.
+    """
+    def __init__(self, players):
+        if len(players) != len(set(players)):
+            raise SamePlayerNameException
 
-    def _reset(self):
-        #next person deals
-        self.dealer += 1
-        self.kitty.empty() #this is important
+        if len(players) != 4 and len(players) != 6:
+            raise NotEnoughPlayersException
 
+        self.players = players
+        #both teams start at 0 points
+        self.team_one = 0
+        self.team_two = 0
+        self.play = None
 
-
+    def start(self):
+        if self.team_one > -500 and self.team_one < 500 and \
+           self.team_two > -500 and self.team_two < 500:
+            self.play = Round(self.players)
